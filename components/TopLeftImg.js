a3f30e086905d43c31f47d520e307861bb2e744e
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-23 12:50:44 -0500

    add image

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -2,7 +2,11 @@
 import Image from "next/image";
 
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return (
+    <div>
+      <Image src='/top-left-img.png' width={400} height={400} alt="" />
+    </div>
+  );
 };
 
 export default TopLeftImg;
