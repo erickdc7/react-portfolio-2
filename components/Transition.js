85b2b514581594020eb24d5f2425134005fc865e
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-23 13:41:06 -0500

    add transition variants

--- components/Transition.js
+++ components/Transition.js
@@ -1,6 +1,22 @@
 // Framer Motion
 import { motion } from "framer-motion";
 
+// Variants
+const transitionVariants = {
+  initial: {
+    x: '100%',
+    width: '100%'
+  },
+  animate: {
+    x: '0%',
+    width: '0%'
+  },
+  exit: {
+    x: ['0%', '100%'],
+    width: ['0%', '100%']
+  }
+}
+
 const Transition = () => {
   return (
     <>
