d954d83ebb89d25134f2923a56a1f4c4b3cf3798
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-23 13:34:43 -0500

    add framer motion

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,9 @@ import Layout from '../components/Layout';
 // Router
 import { useRouter } from 'next/router';
 
+// Framer Motion
+import { AnimatePresence, motion } from 'framer-motion';
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
