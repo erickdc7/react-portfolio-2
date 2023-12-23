8d392982a877da09fe3ad7132ee05a161cf17668
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-23 13:34:06 -0500

    add next router

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import '../styles/globals.css';
 // Components
 import Layout from '../components/Layout';
 
+// Router
+import { useRouter } from 'next/router';
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
