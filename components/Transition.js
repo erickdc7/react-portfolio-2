96a35a293e62eda26928570686fa1ee4b2326eb0
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-23 13:50:58 -0500

    add transition

--- components/Transition.js
+++ components/Transition.js
@@ -28,6 +28,22 @@ const Transition = () => {
         exit="exit"
         transition={{ delay: 0.2, duration: 0.6, ease: 'easeInOut' }}
       ></motion.div >
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4, duration: 0.6, ease: 'easeInOut' }}
+      ></motion.div >
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6, duration: 0.6, ease: 'easeInOut' }}
+      ></motion.div >
     </>
   );
 };
