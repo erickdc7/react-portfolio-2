0c798ba3c5c9afcd727884c456d07dd169740bc1
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-23 12:52:46 -0500

    add image styles

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const TopLeftImg = () => {
   return (
-    <div>
+    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
       <Image src='/top-left-img.png' width={400} height={400} alt="" />
     </div>
   );
