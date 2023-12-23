24832f79d364211cd179a92ba64cc848a05f979d
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-23 12:40:57 -0500

    add font settings

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,13 @@
 // Fonts
 import { Sora } from "@next/font/google"
 
+// Font settings
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+})
+
 const Layout = () => {
   return <div>Layout</div>;
 };
