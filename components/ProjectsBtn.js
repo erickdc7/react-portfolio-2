63aabed7ce3295b89b539cce432bb7216855a389
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 12:30:37 -0500

    add styles

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -10,7 +10,7 @@ import { HiArrowRight } from "react-icons/hi2"
 const ProjectsBtn = () => {
   return (
     <div className="mx-auto xl:mx-0">
-      <Link href={'/work'}>
+      <Link href={'/work'} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
         <Image
           src={'/rounded-text.png'}
           width={141}
@@ -18,7 +18,7 @@ const ProjectsBtn = () => {
           alt=""
           className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
         />
-        <HiArrowRight />
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
       </Link>
     </div>
   );
