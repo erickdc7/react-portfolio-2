c239723b494d72e06348498f2daf3528127c1a57
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 12:48:08 -0500

    add avatar styles

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,7 +2,9 @@
 import Image from "next/image";
 
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className="hidden xl:flex xl:max-w-none">Avatar</div>
+  );
 };
 
 export default Avatar;
