c05a1c91149f249ac93a1dde63c1904b5b970816
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 13:01:44 -0500

    add container styles

--- pages/index.js
+++ pages/index.js
@@ -61,9 +61,16 @@ const Home = () => {
         {/* Particles */}
         <div>Particles</div>
         {/* Avatar Img */}
-        <div className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+        <motion.div
+          variants={fadeIn('up', 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+        >
           <Avatar />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
