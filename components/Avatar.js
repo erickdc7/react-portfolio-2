24182b007c6d8f422467eefd9e3fd5f87e866c3b
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 12:51:47 -0500

    add image styles

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,7 +3,15 @@ import Image from "next/image";
 
 const Avatar = () => {
   return (
-    <div className="hidden xl:flex xl:max-w-none">Avatar</div>
+    <div className="hidden xl:flex xl:max-w-none">
+      <Image
+        src={'/avatar.png'}
+        width={737}
+        height={678}
+        alt=""
+        className="translate-z-0 w-full h-full"
+      />
+    </div>
   );
 };
 
