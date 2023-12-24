8465b01c74650129420acc64032801b69b34989b
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 12:22:56 -0500

    add container styles

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -8,7 +8,11 @@ import Link from "next/link";
 import { HiArrowRight } from "react-icons/hi2"
 
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="mx-auto xl:mx-0">
+      Project Btn
+    </div>
+  );
 };
 
 export default ProjectsBtn;
