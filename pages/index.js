7fc55bf09690dd2590e47fa1d784b74d6d628043
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 11:47:11 -0500

    add components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 // Next image
 import Image from "next/image";
 
+// Components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
 const Home = () => {
   return <div>Home</div>;
 };
