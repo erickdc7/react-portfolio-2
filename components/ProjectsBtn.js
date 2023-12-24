4d308ba6cf4ed9dc697be4a0d0f302ae15af41d3
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 12:25:43 -0500

    add image

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -10,7 +10,15 @@ import { HiArrowRight } from "react-icons/hi2"
 const ProjectsBtn = () => {
   return (
     <div className="mx-auto xl:mx-0">
-      <Link href={'/work'}></Link>
+      <Link href={'/work'}>
+        <Image
+          src={'/rounded-text.png'}
+          width={141}
+          height={148}
+          alt=""
+          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+        ></Image>
+      </Link>
     </div>
   );
 };
