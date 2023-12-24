95dcb79c57aade410326f3f0f1a6f93c8897dd43
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 12:00:25 -0500

    add button

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,10 @@ const Home = () => {
           <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore magnam voluptate minus et id molestiae reprehenderit enim distinctio? Sed temporibus neque aliquid, veritatis facere vel itaque fugiat!
           </p>
+          {/* Btn */}
+          <div>
+            <ProjectsBtn />
+          </div>
         </div>
       </div>
       {/* Image */}
