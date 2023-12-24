6cc50ea6cef5e2f63e503ee4aad97c0cc055a309
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 12:21:38 -0500

    add react icon

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -4,6 +4,9 @@ import Image from "next/image";
 // Next link
 import Link from "next/link";
 
+// Icons
+import { HiArrowRight } from "react-icons/hi2"
+
 const ProjectsBtn = () => {
   return <div>Project Btn</div>;
 };
