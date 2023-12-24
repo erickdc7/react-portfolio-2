6d118e1a28c774b6a68e8e4e0fcbf0ea81542ce4
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 12:45:23 -0500

    add image styles

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ const Home = () => {
       {/* Image */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         {/* Bg Image */}
-        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right"></div>
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
       </div>
     </div>
   );
