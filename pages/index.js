90f8bc5ddc48b49992ea1b04703968022b562b1b
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-24 11:59:29 -0500

    add container styles

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
       {/* Text */}
-      <div>
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* Title */}
           <h1 className="h1">
