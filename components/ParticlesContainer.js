9f18eac2e384d8851af38adc13d9608e59eed40f
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-25 12:37:04 -0500

    add modules

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,3 +1,7 @@
+import { Particles } from "react-tsparticles"
+import { loadFull } from "tsparticles"
+import React, { useCallback } from "react";
+
 const ParticlesContainer = () => {
   return <div>Particles Container</div>;
 };
