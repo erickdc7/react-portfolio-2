ec0e3ef1a0a2e26ca753381d9380237ad435dffe
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-25 12:56:12 -0500

    add header styles

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,9 @@ import Socials from "./Socials";
 
 const Header = () => {
   return (
-    <header className="absolute z-30 w-full">Header</header>
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">Header</div>
+    </header>
   );
 };
 
