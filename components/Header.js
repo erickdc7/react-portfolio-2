a87f13f6bf818466adf8ca6f6b38a654ef11ffa2
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-25 13:01:27 -0500

    add styles

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* Logo */}
           <Link href={'/'}>
             <Image
