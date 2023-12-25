534376b1c81d129de652df428a6b498f1622c2ee
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-25 12:53:37 -0500

    add next link

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
 // Next Image
 import Image from "next/image";
 
+// Next Link
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="absolute z-30 w-full">Header</header>
