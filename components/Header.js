2b5bd31aacffa6429a5a6f9d5448a0cbf367c630
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-25 12:54:21 -0500

    add component

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,9 @@ import Image from "next/image";
 // Next Link
 import Link from "next/link";
 
+// Components
+import Socials from "./Socials";
+
 const Header = () => {
   return (
     <header className="absolute z-30 w-full">Header</header>
