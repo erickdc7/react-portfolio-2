c4bb5ef72012fdcf042ef458bf5e43c5fc05806a
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-25 13:07:33 -0500

    add icons

--- components/Socials.js
+++ components/Socials.js
@@ -10,6 +10,21 @@ const Socials = () => {
       <Link href={''} className="hover:text-accent transition-all duration-300">
         <RiYoutubeLine />
       </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiDribbbleLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiBehanceLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiPinterestLine />
+      </Link>
     </div>
   );
 };
