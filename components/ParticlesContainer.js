e142a01854b6dafc83eb64bbe00ba632f5b811cc
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-25 12:38:39 -0500

    add particles init

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -3,6 +3,11 @@ import { loadFull } from "tsparticles"
 import React, { useCallback } from "react";
 
 const ParticlesContainer = () => {
+  // Init
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine)
+  }, [])
+
   return <div>Particles Container</div>;
 };
 
