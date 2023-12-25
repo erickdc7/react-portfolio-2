6f42e513d24831cdec0ffb076743675b600abf15
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-25 13:04:35 -0500

    add icons

--- components/Socials.js
+++ components/Socials.js
@@ -1,6 +1,9 @@
 // Links
 import Link from "next/link";
 
+// Icons
+import { RiYoutubeLine, RiInstagramLine, RiFacebookLine, RiDribbbleLine, RiBehanceLine, RiPinterestLine } from "react-icons/ri"
+
 const Socials = () => {
   return <div>Socials</div>;
 };
