e67178cf0b48295aabb4a3d8b513f41241f92728
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-25 13:25:43 -0500

    add container styles

--- components/Circles.js
+++ components/Circles.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Circles = () => {
   return (
-    <div>
+    <div className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10">
       <Image
         src={'/circles.png'}
         width={260}
