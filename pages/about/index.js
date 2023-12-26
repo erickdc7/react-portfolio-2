583f2daac828f7fb35ff209b6f6365e803219921
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-26 12:48:13 -0500

    add text

--- pages/about/index.js
+++ pages/about/index.js
@@ -155,6 +155,15 @@ const About = () => {
                   Finished projects
                 </div>
               </div>
+              {/* Projects */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={8} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Winning awards
+                </div>
+              </div>
             </div>
           </div>
         </div>
