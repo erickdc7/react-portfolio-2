e7881ac3f6511f0c439afedc1bde51ae6e20a6c0
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-26 12:08:03 -0500

    add container styles

--- pages/about/index.js
+++ pages/about/index.js
@@ -114,8 +114,8 @@ const About = () => {
         <Avatar />
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
-        <div>text</div>
-        <div>
+        <div className="flex-1 flex flex-col justify-center">text</div>
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {
               aboutData.map((item, itemIndex) => {
@@ -131,7 +131,7 @@ const About = () => {
               })
             }
           </div>
-          <div>
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {
               aboutData[index].info.map((item, itemIndex) => {
                 return (
