330dd210a361557b4a19ecd5bc4c4efa7cc1ae13
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-26 12:41:07 -0500

    add text styles

--- pages/about/index.js
+++ pages/about/index.js
@@ -130,7 +130,7 @@ const About = () => {
             <div>
               {/* Experience */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
-                <div>
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={10} duration={5} /> +
                 </div>
                 <div>Years of experience</div>
