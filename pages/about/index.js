a2de3bd03eb080f0a11fd463b6e7a04aeb60d185
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-26 11:56:54 -0500

    add onClick event

--- pages/about/index.js
+++ pages/about/index.js
@@ -123,6 +123,7 @@ const About = () => {
                   <div
                     key={itemIndex}
                     className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                    onClick={() => setIndex(itemIndex)}
                   >
                     {item.title}
                   </div>
