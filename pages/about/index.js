8551b17c57cf0d97a1dd3e90eee11d7dc01c11cd
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-26 12:50:12 -0500

    update text styles

--- pages/about/index.js
+++ pages/about/index.js
@@ -156,7 +156,7 @@ const About = () => {
                 </div>
               </div>
               {/* Awards */}
-              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <div className="relative flex-1">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={8} duration={5} /> +
                 </div>
