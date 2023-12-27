5e53e2d8628c4b688b4b927e9759d787e2c4ff15
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-27 10:06:05 -0500

    add container

--- pages/services/index.js
+++ pages/services/index.js
@@ -11,7 +11,11 @@ const Services = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
       <Circles />
-      <div className="container mx-auto"></div>
+      <div className="container mx-auto">
+        <div>
+          {/*  */}
+        </div>
+      </div>
       <Bulb />
     </div>
   );
