09b3e5477e0a4380e6b51e2b9ef045ba7d43999d
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-27 10:05:17 -0500

    add container styles

--- components/Bulb.js
+++ components/Bulb.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Bulb = () => {
   return (
-    <div>
+    <div className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px]">
       <Image
         src={'/bulb.png'}
         width={260}
