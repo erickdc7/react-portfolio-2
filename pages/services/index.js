cf50810da20dfe187ba616a1e29cae174f9c7244
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-27 10:00:24 -0500

    add container styles

--- pages/services/index.js
+++ pages/services/index.js
@@ -9,7 +9,7 @@ import { fadeIn } from "../../variants";
 
 const Services = () => {
   return (
-    <div>
+    <div className="h-full bg-primary/30 py-36 flex items-center">
       <Circles />
       <div className="container mx-auto"></div>
       <Bulb />
