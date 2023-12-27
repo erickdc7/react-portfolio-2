87e9e4f677ef04c091dd18f2255a539a9d2d000a
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-27 09:50:30 -0500

    add framer motion

--- pages/services/index.js
+++ pages/services/index.js
@@ -3,6 +3,9 @@ import ServiceSlider from "../../components/ServiceSlider";
 import Bulb from "../../components/Bulb";
 import Circles from "../../components/Circles";
 
+// Framer Motion
+import { motion } from "framer-motion";
+
 const Services = () => {
   return <div>Services</div>;
 };
