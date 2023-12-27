9dbfac18cf98a924873d77fee83cdb7256cce8e9
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-27 09:49:31 -0500

    add components

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,3 +1,8 @@
+// Components
+import ServiceSlider from "../../components/ServiceSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+
 const Services = () => {
   return <div>Services</div>;
 };
