059d381114c4f7f6c74e31575da9f8d3b20ffcfc
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-27 10:03:09 -0500

    add image

--- components/Bulb.js
+++ components/Bulb.js
@@ -2,7 +2,17 @@
 import Image from "next/image";
 
 const Bulb = () => {
-  return <div>Bulb</div>;
+  return (
+    <div>
+      <Image
+        src={'/bulb.png'}
+        width={260}
+        height={200}
+        className="w-full h-full"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Bulb;
