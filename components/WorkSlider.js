143cc1cd19986a4e9cbb780b0e2022305a09c4ef
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-28 12:41:29 -0500

    add title styles

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -94,7 +94,7 @@ const WorkSlider = () => {
                           {/* Overlay gradient */}
                           <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                           {/* Title */}
-                          <div>
+                          <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
                             <div>
                               {/* Title Part 1 */}
                               <div>title part 1</div>
