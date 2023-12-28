b3bd270a013496e0a8c526b5d64f7207588d3237
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-28 12:27:26 -0500

    add comments

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -93,6 +93,7 @@ const WorkSlider = () => {
                           />
                           {/* Overlay gradient */}
                           <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                          {/* Title */}
                         </div>
                       </div>
                     )
