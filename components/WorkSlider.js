e3a15b19a60a70e541cb831076cba07155bcce87
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-28 12:12:05 -0500

    update content

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -79,28 +79,10 @@ const WorkSlider = () => {
       className="h-[240px] sm:h-[340px]"
     >
       {
-        serviceData.map((item, index) => {
+        workSlides.slides.map((item, index) => {
           return (
             <SwiperSlide key={index}>
-              <div className="bg-[rgba(65,47,125,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
-                {/* Icon */}
-                <div className="text-4xl text-accent mb-4">
-                  {item.icon}
-                </div>
-                {/* Title & Desc */}
-                <div className="mb-8">
-                  <div className="mb-2 text-lg">
-                    {item.title}
-                  </div>
-                  <p className="max-w-[350px] leading-normal">
-                    {item.description}
-                  </p>
-                </div>
-                {/* Arrow */}
-                <div className="text-3xl">
-                  <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
-                </div>
-              </div>
+
             </SwiperSlide>
           )
         })
