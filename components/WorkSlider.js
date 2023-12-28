12dbf8f580e3b295169b7efb7e76df500010e9db
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-28 12:16:07 -0500

    add image

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -78,6 +78,7 @@ const WorkSlider = () => {
                     return (
                       <div key={index}>
                         {/* Image */}
+                        <Image />
                       </div>
                     )
                   })
