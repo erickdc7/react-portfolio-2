ac18f471cce9ecc23dc316be5c3fdaa42836a20d
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-28 11:25:20 -0500

    add text animation

--- pages/services/index.js
+++ pages/services/index.js
@@ -15,9 +15,15 @@ const Services = () => {
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* Text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8">
+            <motion.h2
+              variants={fadeIn('up', 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
               My services <span className="text-accent">.</span>
-            </h2>
+            </motion.h2>
             <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores accusantium excepturi exercitationem sit minima.
             </p>
