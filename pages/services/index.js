3a899b2bee1f61a3f683d168ca640d8f2b24ffd6
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-28 11:27:18 -0500

    add text animation

--- pages/services/index.js
+++ pages/services/index.js
@@ -16,7 +16,7 @@ const Services = () => {
           {/* Text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
             <motion.h2
-              variants={fadeIn('up', 0.3)}
+              variants={fadeIn('up', 0.2)}
               initial="hidden"
               animate="show"
               exit="hidden"
@@ -24,9 +24,15 @@ const Services = () => {
             >
               My services <span className="text-accent">.</span>
             </motion.h2>
-            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+            <motion.p
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores accusantium excepturi exercitationem sit minima.
-            </p>
+            </motion.p>
           </div>
           <div className="w-full xl:max-w-[65%]">
             {/* Slider */}
