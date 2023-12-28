5bc6e9f180d6ba315b110a9c8d03e0d9d9cb6b2f
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-28 12:02:55 -0500

    update files

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,51 +1,74 @@
-// data
-const workSlides = {
-  slides: [
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-      ],
-    },
-  ],
-};
+// Swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// Icons
+import {
+  RxCrop,
+  RxPencil2,
+  RxDesktop,
+  RxReader,
+  RxRocket,
+  RxArrowRight,
+  RxArrowTopRight,
+} from "react-icons/rx";
+
+// Required modules
+import { FreeMode, Pagination } from "swiper";
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true
+      }}
+      modules={[FreeMode, Pagination]}
+      className="h-[240px] sm:h-[340px]"
+    >
+      {
+        serviceData.map((item, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="bg-[rgba(65,47,125,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+                {/* Icon */}
+                <div className="text-4xl text-accent mb-4">
+                  {item.icon}
+                </div>
+                {/* Title & Desc */}
+                <div className="mb-8">
+                  <div className="mb-2 text-lg">
+                    {item.title}
+                  </div>
+                  <p className="max-w-[350px] leading-normal">
+                    {item.description}
+                  </p>
+                </div>
+                {/* Arrow */}
+                <div className="text-3xl">
+                  <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
+                </div>
+              </div>
+            </SwiperSlide>
+          )
+        })
+      }
+    </Swiper>
+  );
 };
 
 export default WorkSlider;
--- pages/work/index.js
+++ pages/work/index.js
@@ -22,7 +22,7 @@ const Work = () => {
               exit="hidden"
               className="h2 xl:mt-8"
             >
-              My services <span className="text-accent">.</span>
+              My work <span className="text-accent">.</span>
             </motion.h2>
             <motion.p
               variants={fadeIn('up', 0.4)}
