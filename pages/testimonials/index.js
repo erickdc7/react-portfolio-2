c7fe4aed09f645847dfc6c9f7ad062065bb33ef2
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-29 12:47:54 -0500

    add container styles

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -4,7 +4,7 @@ import TestimonialSlider from "../../components/TestimonialSlider";
 const Testimonials = () => {
   return (
     <div className="h-full bg-primary/30 py-32 text-center">
-      <div>
+      <div className="container mx-auto h-full flex flex-col justify-center">
         <h2>What clients <span className="text-accent">say.</span></h2>
       </div>
     </div>
