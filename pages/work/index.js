d1836caeecd762e92db859050925da4c27585700
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-29 12:36:26 -0500

    add title styles

--- pages/work/index.js
+++ pages/work/index.js
@@ -20,7 +20,7 @@ const Work = () => {
               initial="hidden"
               animate="show"
               exit="hidden"
-              className="h2 xl:mt-8"
+              className="h2 xl:mt-12"
             >
               My work <span className="text-accent">.</span>
             </motion.h2>
