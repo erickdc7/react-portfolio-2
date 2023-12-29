b1454a466465d5168bea6515290d669c892572c4
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-29 12:49:32 -0500

    add comments

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -5,6 +5,7 @@ const Testimonials = () => {
   return (
     <div className="h-full bg-primary/30 py-32 text-center">
       <div className="container mx-auto h-full flex flex-col justify-center">
+        {/* Title */}
         <h2 className="h2 mb-8 xl:mb-0">
           What clients <span className="text-accent">say.</span>
         </h2>
