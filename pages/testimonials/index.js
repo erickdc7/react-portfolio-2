2fb106de02d060e98eeccd9715afa40b3016896f
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-30 11:50:17 -0500

    add text animation

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,14 +1,24 @@
 // Components
 import TestimonialSlider from "../../components/TestimonialSlider";
 
+// Framer Motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Testimonials = () => {
   return (
     <div className="h-full bg-primary/30 py-32 text-center">
       <div className="container mx-auto h-full flex flex-col justify-center">
         {/* Title */}
-        <h2 className="h2 mb-8 xl:mb-0">
+        <motion.h2
+          variants={fadeIn('up', 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="h2 mb-8 xl:mb-0"
+        >
           What clients <span className="text-accent">say.</span>
-        </h2>
+        </motion.h2>
         {/* Slider */}
         <div>
           <TestimonialSlider />
