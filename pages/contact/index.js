c4a495913fe46afa25d021eb542c9d4691c511a0
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-30 12:16:47 -0500

    add framer motion

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -4,6 +4,9 @@ import Circles from "../../components/Circles";
 // Icons
 import { BsArrowRight } from "react-icons/bs";
 
+// Framer Motion
+import { motion } from "framer-motion";
+
 const Contact = () => {
   return <div>Contact</div>;
 };
