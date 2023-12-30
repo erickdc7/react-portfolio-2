914bbb7c3bc26eb969829848ca743995a58e6464
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-30 11:41:53 -0500

    add container styles

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -59,7 +59,7 @@ const TestimonialSlider = () => {
                 <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
                   <div className="flex flex-col justify-center text-center">
                     {/* Avatar */}
-                    <div>
+                    <div className="mb-2 mx-auto">
                       <Image src={person.image} width={100} height={100} alt="" />
                     </div>
                     {/* Name */}
