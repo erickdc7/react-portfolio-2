a85ed355c38694b6869337b28281a1d004cac05e
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-30 11:38:05 -0500

    update container styles

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -67,7 +67,7 @@ const TestimonialSlider = () => {
                   </div>
                 </div>
                 {/* Quote & Message */}
-                <div className="bg-pink-500/10 flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
+                <div className="flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
                   {/* Quote Icon */}
                   <div className="mb-4">
                     <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
