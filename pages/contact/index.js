ff803271811f04055f3e9b958f00c7034256be5d
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-31 13:23:05 -0500

    fix text

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -24,7 +24,7 @@ const Contact = () => {
             exit="hidden"
             className="h2 text-center mb-12"
           >
-            Let's <span className="text-accent">connect.</span>
+            Let&apos;s <span className="text-accent">connect.</span>
           </motion.h2>
           {/* Form */}
           <motion.form
@@ -43,7 +43,7 @@ const Contact = () => {
             <textarea placeholder="message" className="textarea"></textarea>
             <button className="button rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
               <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
-                Let's talk
+                Let&apos;s talk
               </span>
               <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
             </button>
