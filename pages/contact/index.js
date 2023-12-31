5bb1472e21128bbd1db1737c466ecb840f7f13dc
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-12-31 13:15:40 -0500

    add icon styles

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -32,8 +32,8 @@ const Contact = () => {
             <button className="button rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
               <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
                 Let's talk
-                <BsArrowRight />
               </span>
+              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
             </button>
           </form>
         </div>
